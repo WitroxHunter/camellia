01faf71b15b54305666d8073f6f6afd376c8c37b
Wiktor <80409661+WitroxHunter@users.noreply.github.com>
2021-10-09 21:49:05 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+
+function pokazsociale() {
+  var x = document.getElementById("sociale");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function muzyka() {
+    var m = document.getElementById("muzyka");
+    if (m.style.display === "none") {
+      m.style.display = "block";
+    } else {
+      m.style.display = "none";
+    }
+  }
